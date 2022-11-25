6722cd332e99ea611cb01c13432dbbf4379665d8
Ejay02 <e.jae02@gmail.com>
2022-11-25 06:25:50 +0100

    chat server using websocket and nestjs: create, join, typing and find all messages implemented.

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { Message } from './entities/message.entity';
+
+@Injectable()
+export class MessagesService {
+  messages: Message[] = [{ name: 'Ghost x', text: 'Yo!!' }];
+  clientToUser = {};
+
+  identify(name: string, clientId: string) {
+    this.clientToUser[clientId] = name;
+
+    return Object.values(this.clientToUser);
+  }
+
+  getClientName(clientId: string) {
+    return this.clientToUser[clientId];
+  }
+
+  // create(createMessageDto: CreateMessageDto) {
+  //   const message = {
+  //     ...createMessageDto,
+  //   };
+
+  create(createMessageDto: CreateMessageDto, clientId: string) {
+    const message = {
+      name: this.clientToUser[clientId],
+      text: createMessageDto.text,
+    };
+
+    this.messages.push(message);
+    return message;
+  }
+
+  findAll() {
+    return this.messages;
+  }
+}
